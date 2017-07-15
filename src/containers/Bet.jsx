f69b3a19a68ad3f568817406b1e361b5940bae75
Paul Rowe <prowe214@gmail.com>
2017-07-14 21:15:43 -0600

    static placeholder info

--- src/containers/Bet.jsx
+++ src/containers/Bet.jsx
@@ -21,18 +21,30 @@ class Bet extends Component {
               <div className="contender">
                 <h2></h2>
                 <div className="contender-info">
-                  <span className="info-left">
-
-                  </span>
+                  <span className="info-left">Betters</span><span className="info-right">54</span>
+                  <span className="info-left">Contender Pot</span><span className="info-right">254</span>
                 </div>
               </div>
 
               <div className="contender">
                 <h2>[[CONTENDER TITLE]]</h2>
+                <div className="contender-info">
+                  <span className="info-left">Betters</span><span className="info-right">34</span>
+                  <span className="info-left">Contender Pot</span><span className="info-right">143</span>
+                </div>
               </div>
             </div>
 
-            <div className="action-container"></div>
+            <div className="action-container">
+              <h2>Get in on this Bet!</h2>
+              <input type="text" placeholder="Your Wallet Key..."/>
+              <input type="text" placeholder="Your Bet Amount..."/>
+              <a href="#">
+                <div className="place-bet">
+                  Place Your Bet!
+                </div>
+              </a>
+            </div>
 
           </div>
         </div>
