aa7d9ae127ccfa45c4fcdc3e0164d08f1d5e8807
Paul Rowe <prowe214@gmail.com>
2017-07-14 21:06:45 -0600

    basic containers

--- src/containers/Bet.jsx
+++ src/containers/Bet.jsx
@@ -5,11 +5,30 @@ class Bet extends Component {
     return (
       <div className="pure-g">
         <div className="pure-u-1-1">
-          <h1>Good to Go!</h1>
-          <p>Your Truffle Box is installed and ready.</p>
-          <h2>Smart Contract Example</h2>
-          <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-          <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
+          <div className="bet-box">
+
+            <div className="title-container">
+              <h1>[[BET TITLE]]</h1>
+            </div>
+            
+            <div className="contenders-container">
+              <div className="contender">
+                <h2>[[CONTENDER TITLE]]</h2>
+                <div className="contender-info">
+                  <span className="info-left">
+                    
+                  </span>
+                </div>
+              </div>
+
+              <div className="contender">
+                <h2>[[CONTENDER TITLE]]</h2>
+              </div>
+            </div>
+
+            <div className="action-container"></div>
+
+          </div>
         </div>
       </div>
     );
