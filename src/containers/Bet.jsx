299f81aa6203b796da921a1446dee230688afeec
Sean Michael <seankmichael@gmail.com>
2017-07-14 21:15:23 -0600

    add default props

--- src/containers/Bet.jsx
+++ src/containers/Bet.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Bet extends Component {
+  constructor() {
+
+  }
+
   render() {
+    const { title, positiveItem, negativeItem } = this.props;
     return (
       <div className="pure-g">
         <div className="pure-u-1-1">
           <div className="bet-box">
 
             <div className="title-container">
-              <h1>[[BET TITLE]]</h1>
+              <h1>{title}</h1>
             </div>
             
             <div className="contenders-container">
               <div className="contender">
-                <h2>[[CONTENDER TITLE]]</h2>
+                <h2></h2>
                 <div className="contender-info">
                   <span className="info-left">
                     
@@ -33,6 +39,24 @@ class Bet extends Component {
       </div>
     );
   }
+}
+
+Bet.PropTypes = {
+  title: PropTypes.string,
+  positiveItem: PropTypes.shape({
+    total: PropTypes.number
+  }),
+  negativeItem: PropTypes.shape({
+    total: PropTypes.number
+  }),
+  publicKey: PropTypes.string
+};
+
+Bet.defaultProps = {
+  title: 'Bet Title',
+  positiveItem: { total: 12414 },
+  negativeItem: { total: 423 },
+  publicKey: "0x251b693b329ec942783ab084eae4dc9c613766f9"
 }
 
 export default Bet;
